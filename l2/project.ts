8bda9c58176764ab8e93a19dab70b2252af6375f
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-10 08:37:52 -0300

    first update

---
+++ l2/project.ts
@@ -0,0 +1,15 @@
+/// <mls shortName="project" project="102028" enhancement="_100554_enhancementLit" groupName="other" />
+
+export const projectConfig = {
+    masterFrontEnd: {
+        build: '',
+        start: '',
+        liveView: '',
+    },
+    masterBackEnd: {
+        build: '',
+        start: '',
+        serverView: ''
+    },
+    modules: []
+}
